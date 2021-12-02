95e919266d0648ee72de5df9cb77a3a3f1a1b5c6
Luis Chang <luis.chang@outlook.com>
2021-12-01 22:22:12 -0500

    Finish CRUD JQUERY Customer

--- M4NETCORE_MVCEF_JQUERY.Web/Controllers/CustomerController.cs
+++ M4NETCORE_MVCEF_JQUERY.Web/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using M4NETCORE_MVCEF_JQUERY.Web.Repository;
+﻿using M4NETCORE_MVCEF_JQUERY.Web.Models;
+using M4NETCORE_MVCEF_JQUERY.Web.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,10 +15,55 @@ namespace M4NETCORE_MVCEF_JQUERY.Web.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Eliminar(int idCliente)
+        {
+            var exito = await CustomerRepo.Delete(idCliente);
+            return Json(exito);
+        }
+
+        public async Task<IActionResult> Obtener(int idCliente)
+        {
+            var customer = await CustomerRepo.GetCustomerById(idCliente);
+            return Json(customer);
+        }
+
         public async Task<IActionResult> Listado()
         {
             var customers = await CustomerRepo.GetCustomersAsync();
             return PartialView(customers);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Grabar(int idCliente, string nombres, string apellidos, string pais,
+                                                string ciudad, string telefono)
+        {
+            bool exito = true;
+
+            var customer = new Customer()
+            {
+                FirstName = nombres,
+                LastName = apellidos,
+                Country = pais,
+                City = ciudad,
+                Phone = telefono
+            };
+
+            if (idCliente == -1)
+            {
+                //Es un nuevo cliente
+                exito = await CustomerRepo.Insert(customer);
+            }
+            else
+            {
+                //Es una actualización
+                customer.Id = idCliente;
+                exito = await CustomerRepo.Update(customer);
+            }
+
+            return Json(exito);
+        }
+
+
+
     }
 }
