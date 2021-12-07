291b56dee60005c213d3ddb117de7ce9cd830ad5
Luis Chang <luis.chang@outlook.com>
2021-12-06 21:08:24 -0500

    Consuming API

--- M4NETCORE_MVCEF_JQUERY.Web/Repository/CustomerRepo.cs
+++ M4NETCORE_MVCEF_JQUERY.Web/Repository/CustomerRepo.cs
@@ -1,8 +1,11 @@
 ﻿using M4NETCORE_MVCEF_JQUERY.Web.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace M4NETCORE_MVCEF_JQUERY.Web.Repository
@@ -23,57 +26,84 @@ namespace M4NETCORE_MVCEF_JQUERY.Web.Repository
 
         public static async Task<IEnumerable<Customer>> GetCustomersAsync()
         {
-            using var data = new SalesDB2021Context();
-            return await data.Customer.ToListAsync();
-
+            //using var data = new SalesDB2021Context();
+            //return await data.Customer.ToListAsync();
+            using var httpClient = new HttpClient();
+            using var response = await httpClient
+                 .GetAsync("http://localhost:22846/api/customer/customer");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var customers = JsonConvert.DeserializeObject<IEnumerable<Customer>>(apiResponse);
+            return customers;
         }
 
         public static async Task<Customer> GetCustomerById(int id)
         {
-            using var data = new SalesDB2021Context();
+            //using var data = new SalesDB2021Context();
 
-            return await data.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+            //return await data.Customer.Where(x => x.Id == id).FirstOrDefaultAsync();
+            using var httpClient = new HttpClient();
+            using var response = await httpClient
+                 .GetAsync("http://localhost:22846/api/customer/customerbyid?id=" + id);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var customer = JsonConvert.DeserializeObject<Customer>(apiResponse);
+            return customer;
         }
 
         public static async Task<bool> Insert(Customer customer)
         {
-            bool exito = true;
-            try
-            {
-                using var data = new SalesDB2021Context();
-                await data.Customer.AddAsync(customer);
-                await data.SaveChangesAsync();
-            }
-            catch (Exception)
-            {
-                exito = false;
-            }
+            //bool exito = true;
+            //try
+            //{
+            //    using var data = new SalesDB2021Context();
+            //    await data.Customer.AddAsync(customer);
+            //    await data.SaveChangesAsync();
+            //}
+            //catch (Exception)
+            //{
+            //    exito = false;
+            //}
 
-            return exito;
+            var json = JsonConvert.SerializeObject(customer);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            using var httpClient = new HttpClient();
+            using var response = await httpClient
+                   .PostAsync("http://localhost:22846/api/customer/postcustomer", data);
+            string apiresponse = await response.Content.ReadAsStringAsync();
+            var customerResponse = JsonConvert.DeserializeObject<bool>(apiresponse);
+            return customerResponse;
         }
 
         public static async Task<bool> Update(Customer customer)
         {
-            bool exito = true;
-            try
-            {
-                using var data = new SalesDB2021Context();
-                var customerDB = await data.Customer.Where(x => x.Id == customer.Id).FirstOrDefaultAsync();
-                customerDB.FirstName = customer.FirstName;
-                customerDB.LastName = customer.LastName;
-                customerDB.City = customer.City;
-                customerDB.Country = customer.Country;
-                customerDB.Phone = customer.Phone;
+            //    bool exito = true;
+            //    try
+            //    {
+            //        using var data = new SalesDB2021Context();
+            //        var customerDB = await data.Customer.Where(x => x.Id == customer.Id).FirstOrDefaultAsync();
+            //        customerDB.FirstName = customer.FirstName;
+            //        customerDB.LastName = customer.LastName;
+            //        customerDB.City = customer.City;
+            //        customerDB.Country = customer.Country;
+            //        customerDB.Phone = customer.Phone;
 
-                await data.SaveChangesAsync();
+            //        await data.SaveChangesAsync();
 
-            }
-            catch (Exception)
-            {
-                exito = false;
-            }
+            //    }
+            //    catch (Exception)
+            //    {
+            //        exito = false;
+            //    }
 
-            return exito;
+            var json = JsonConvert.SerializeObject(customer);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            using var httpClient = new HttpClient();
+            using var response = await httpClient
+                   .PutAsync("http://localhost:22846/api/customer/putcustomer", data);
+            string apiresponse = await response.Content.ReadAsStringAsync();
+            var customerResponse = JsonConvert.DeserializeObject<bool>(apiresponse);
+            return customerResponse;
+
+            //return exito;
         }
         /// <summary>
         /// Delete Customers by ID
@@ -82,21 +112,31 @@ namespace M4NETCORE_MVCEF_JQUERY.Web.Repository
         /// <returns></returns>
         public static async Task<bool> Delete(int id)
         {
+            //bool exito = true;
+            //try
+            //{
+            //    using var data = new SalesDB2021Context();
+            //    var customerDB = await data.Customer.FindAsync(id);
+            //    data.Customer.Remove(customerDB);
+            //    await data.SaveChangesAsync();
+            //}
+            //catch (Exception)
+            //{
+
+            //    exito = false;
+            //}
+
+            //return exito;
+
             bool exito = true;
-            try
-            {
-                using var data = new SalesDB2021Context();
-                var customerDB = await data.Customer.FindAsync(id);
-                data.Customer.Remove(customerDB);
-                await data.SaveChangesAsync();
-            }
-            catch (Exception)
-            {
-
+            using var httpClient = new HttpClient();
+            using var response = await httpClient
+                .DeleteAsync("http://localhost:22846/api/customer/deletecustomer?id=" + id);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            if ((int)response.StatusCode == 400)
                 exito = false;
-            }
-
             return exito;
+
         }
 
     }
